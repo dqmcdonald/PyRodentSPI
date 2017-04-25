bec96865142fa8e9bd9ec35f7ecf74e84a6a2a9f
Quentin McDonald <dqmcdonald@gmail.com>
2017-04-25 02:49:15 +0000

    Added some TODO comments.

--- RodentSPI.py
+++ RodentSPI.py
@@ -1,9 +1,33 @@
+# TODO: Add comments about what this script does
+# TODO: Add names of authors and copyright
+
+#TODO: import time and camera modules
+
+
 import serial
 
+# TODO: Write a function that returns a filename in the form of a timestamp.
+# This should be guarenteed to be unique so we never overwrite any files.
+# It also should not include any spaces. Hint: time.strftime()
+# Example might be: 
+# def timestamp_filename():
+#     fname = <some code here>
+#     return fname
 
-print "Start"
+
+# TODO: Comment on what is happening here:
 ser = serial.Serial('/dev/ttyACM0')
 
+
+# TODO: Comment on what is happening here:
 while 1 :
     line = ser.readline()
-    print line
+
+#   TODO: Look at line and decide if we need to take a photo:
+
+
+#   TODO: Take the photo and save it to a file
+
+
+
+
